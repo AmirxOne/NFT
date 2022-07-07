a61ea8e29f2abefc8c9613934e9f3256bdd96eae
Amir.x.One <Amir.x.one.2000@gmail.com>
2022-07-07 10:38:49 +0430

    side Navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -36,6 +36,7 @@ const Header = () => {
             return setBg(null)
         }
         setBg(i)
+        setShow(!show)
     }
 
     const [show, setShow] = useState(false);
@@ -51,17 +52,19 @@ const Header = () => {
                     <h1 className='text-4xl font-extrabold'><span></span>NFT.</h1>
                 </div>
                
-                <ul className='hidden md:flex items-center justify-center text-white text-lg'>
+                <ul className={show ? 'a -right-0' : 'a -right-[1000px]'}>
                     {
-                        data_Nav.map((item, i) => <li className={bg === i ? "text-[#e250e5]" : "text-white"} onClick={() => clickHandler(i) } key={item.id}>
+                        data_Nav.map((item, i) => <li className={bg === i ? "text-[#e250e5] mb-12 md:mb-0" : "text-white mb-12 md:mb-0"} 
+                        onClick={() => clickHandler(i) } key={item.id}>
                             <Link className='px-4 py-2 ' to={item.url}>{item.navTitle}</Link>
                             </li>)
                     }
+                    <div onClick={showHandler} className='md:hidden absolute top-4 left-4'><AiOutlineClose className='text-2xl text-white'/></div>
                 </ul>
                 
                 <div className='hidden md:flex hover:shadow-[0_0_10px] hover:shadow-[#4b50e6] focus: border border-[#4b50e6]  overflow-hidden rounded-3xl text-white'><Link className='px-5 py-2 text-sm w-full h-full flex items-center justify-center' to='/wallet'><FaWallet className='mr-2'/>Connect Wallet</Link></div>
                 <div onClick={showHandler} className='flex md:hidden'>
-                    {show ? <AiOutlineClose className='text-2xl text-white'/> : <AiOutlineMenu className='text-2xl text-white'/> }   
+                    <AiOutlineMenu className='text-2xl text-white'/>  
                 </div>
             </div>
         </header>
