3a245c8e0c77fcc04d2603dcf34d596701063b9e
Amir.x.One <Amir.x.one.2000@gmail.com>
2022-07-08 12:54:08 +0430

    Edite Z-index Navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -36,7 +36,7 @@ const Header = () => {
     }
 
     return (
-        <header className='z-[1000] w-full h-[70px] fixed top-0 bg-[#14141fcb] backdrop-blur-[12px]'>
+        <header className=' z-[1000] w-full h-[70px] fixed top-0 bg-[#14141fcb] backdrop-blur-[12px]'>
             <div className='max-w-[640px] md:max-w-[900px] lg:max-w-[1024px] xl:max-w-[1320px] mx-auto h-full px-4 flex items-center justify-between '>
                 <div className='flex items-center bg-clip-text text-transparent bg-gradient-to-tr from-[#4b50e6] via-[#e250e5] to-[#4b50e6]'>
                     <AiOutlineFire className='text-5xl text-[#e250e5]'/>
@@ -45,7 +45,7 @@ const Header = () => {
                
                 <ul className={show ? 'a -right-0' : 'a -right-[1000px]'}>
                     {
-                        data_Nav.map((item, i) => <li className='mb-12 md:mb-0' 
+                        data_Nav.map((item) => <li className='mb-12 md:mb-0' 
                         onClick={showHandler} key={item.id}>
                             <NavLink  className={(navClass) => navClass.isActive ? 'px-4 py-2 text-[#e250e5] ' : 'px-4 py-2' } to={item.url}>{item.navTitle}</NavLink > 
                             {/* ({isActive}) => isActive ? '' : '' */}
