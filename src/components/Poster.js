895794fbeee8c8de4c1d907c6f71905df18d5615
Amir.x.One <Amir.x.one.2000@gmail.com>
2022-07-08 18:59:55 +0430

    updiateing

--- src/components/Poster.js
+++ src/components/Poster.js
@@ -10,7 +10,7 @@ const Poster = () => {
     console.log(loc)
 
     return (
-        <div className='w-full h-64 relative -top-[70px] relative'>
+        <div className='w-full h-64 -top-[70px] relative'>
             <img className='object-cover h-64 w-full' src={bgContact} alt="" />
             <div className='text-white absolute top-1/2 left-[44%] font-extrabold text-4xl '>
                 {loc.pathname === "/contact" ? <span>Contact</span> : <span>Create Item</span>}
