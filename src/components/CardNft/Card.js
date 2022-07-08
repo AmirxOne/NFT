3fe28c0a13103ff4a667df9ce62ccfdebc0f34cb
Amir.x.One <Amir.x.one.2000@gmail.com>
2022-07-08 12:24:07 +0430

    test

--- src/components/CardNft/Card.js
+++ src/components/CardNft/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { AiOutlineWallet } from 'react-icons/ai';
 
 const Card = ({data}) => {
-    const {title, dis, url} = data
+    const {title, dis} = data
     return (
         <div className='xl:col-span-3 lg:col-span-4 sm:col-span-6 col-span-12  bg-[#343444] rounded-lg flex flex-col text-white p-7 mb-7 sm:mb-0'>
             <div>
