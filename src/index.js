0e7617a51fd20a63aaca474a2c5fc5d8160ffb3d
whh32 <am.lashkari2017@gmail.com>
2022-07-10 19:02:18 +0430

    update

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+
 // react router dom
 import {BrowserRouter} from 'react-router-dom'
 
