16f4d9a50dece950970c47319457c7d05e3d0ccc
whh32 <am.lashkari2017@gmail.com>
2022-07-10 16:53:29 +0430

    update

--- src/components/TrendNft/Trends.js
+++ src/components/TrendNft/Trends.js
@@ -26,7 +26,7 @@ const Trends = (props) => {
   }
 
   return (
-    <>
+    <Fragment>
       <Payment state={stateBox} onCloseBox={closeBox} />
       <div className='xl:max-w-[1320px] lg:max-w-[1024px] md:max-w-[900px] sm:max-w-[640px] px-4 h-auto m-auto my-[100px]'>
         <span className='font-medium text-[30px] text-[#ffffff] flex mb-8'>Trend</span>
@@ -89,48 +89,7 @@ const Trends = (props) => {
             ))}
         </div>
       </div>
-
-      <div className='grid grid-cols-1 s:grid-cols-2 gap-y-10 sm:grid-cols-3 gap-x-6 lg:grid-cols-4 xl:grid-cols-4 xl:gap-x-8'>
-        {select === 'high' && nftData.filter((data) => data.title.toLowerCase().includes(search.toLowerCase()))
-          .sort((a, b) => b.currentBid - a.currentBid)
-          .map(data =>
-            <Trend
-              key={data.id}
-              title={data.title}
-              creator={data.creator}
-              image={data.imgUrl}
-              avatar={data.creatorImg}
-              price={data.currentBid}
-              id={data.id}
-            />)}
-
-        {select === 'low' && nftData.filter(data => data.title.toLowerCase().includes(search.toLowerCase()))
-          .sort((a, b) => a.currentBid - b.currentBid)
-          .map((data) =>
-            <Trend
-              key={data.id}
-              title={data.title}
-              creator={data.creator}
-              image={data.imgUrl}
-              avatar={data.creatorImg}
-              price={data.currentBid}
-              id={data.id}
-            />)}
-
-        {select === 'default' && nftData.filter(data => data.title.toLowerCase().includes(search.toLowerCase()))
-          .map((data) => (
-            <Trend
-              key={data.id}
-              title={data.title}
-              creator={data.creator}
-              image={data.imgUrl}
-              avatar={data.creatorImg}
-              price={data.currentBid}
-              id={data.id}
-            />
-          ))}
-      </div>
-    </>
+    </Fragment>
   )
 }
 
