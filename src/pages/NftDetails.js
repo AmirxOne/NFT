4ddea63237cdb50e413eb77ed8b9681c950a1ffa
Amir.x.One <Amir.x.one.2000@gmail.com>
2022-07-10 15:48:02 +0430

    Responsiv Ditels

--- src/pages/NftDetails.js
+++ src/pages/NftDetails.js
@@ -17,7 +17,7 @@ const NftDetails = () => {
     const id = params.id - 1;
 
     const dataD = nftData.find(item => item.id === id + 1)
-    const {title, desc, imgUrl, creator, creatorImg, currentBid} = dataD
+    const {title, desc, imgUrl, creator, creatorImg} = dataD
 
     //cuntionall
     const [show ,setShow] = useState(false);
@@ -28,12 +28,12 @@ const NftDetails = () => {
     return (
             <>
                 <Poster/>
-                <div className='text-white w-full mt-28 lg:mt-32'>
+                <div className='text-white w-full lg:mt-32'>
                     <div className='max-w-[640px] md:max-w-[900px] lg:max-w-[1024px] xl:max-w-[1320px] mx-auto h-full px-4 flex flex-col lg:flex-row items-center justify-center'>
-                        <div className='w-[600px] h-[600px] rounded-2xl overflow-hidden'><img src={imgUrl} alt="NFT_Image" /></div>
-                        <div className='w-1/2 text-white p-8'>
-                            <h1 className='font-bold text-4xl mb-8'>{title}</h1>
-                            <div className='flex items-center justify-between text-white mb-8'>
+                        <div className='w-[290px] md:w-[600px] rounded-2xl overflow-hidden'><img src={imgUrl} alt="NFT_Image" /></div>
+                        <div className='w-full lg:w-1/2 flex flex-col items-center justify-center lg:items-start text-white p-2 md:p-8 md:px-16 lg:px-8'>
+                            <h1 className='font-bold text-4xl mt-5 md:mt-0 mb-8'>{title}</h1>
+                            <div className='w-full flex items-center justify-between text-white mb-5 xl:mb-8'>
                                 <div className='flex items-center justify-center'>
                                     <span className='rounded-full bg-black h-7 w-7 flex items-center justify-center mr-1 cursor-pointer'><AiOutlineEye className='text-sm'/></span>
                                     234
@@ -55,8 +55,8 @@ const NftDetails = () => {
                                     <span className='font-medium'>{creator}</span>
                                 </div>
                             </div>
-                            <p className='mb-10 text-stone-200 leading-9'>{desc}</p>
-                            <Link className='flex font-medium items-center justify-center bg-[#5142fc] hover:bg-white hover:ring-2 hover:ring-[#5142fc] hover:text-[#5142fc] transition-all duration-300 rounded-full py-2' to='/wallet'><BsBasket className='mr-2'/>Place a Bid</Link>
+                            <p className='mb-10 text-stone-200 leading-9 text-center md:text-left'>{desc}</p>
+                            <Link className='w-full flex font-medium items-center justify-center bg-[#5142fc] hover:bg-white hover:ring-2 hover:ring-[#5142fc] hover:text-[#5142fc] transition-all duration-300 rounded-full py-2' to='/wallet'><BsBasket className='mr-2'/>Place a Bid</Link>
                         </div>
                     </div>
                 </div>
